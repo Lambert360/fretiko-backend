fbc3bd5e6faa366bdea945633d567d0e48c17914
Lambert360 <victoryambert360@gmail.com>
2026-04-07 18:19:02 +0100

    Fix JwtService dependency injection in AuctionsModule

--- src/auctions/auctions.module.ts
+++ src/auctions/auctions.module.ts
@@ -10,6 +10,7 @@ import { AuctionFraudDetectionService } from './fraud-detection.service';
 import { WalletModule } from '../wallet/wallet.module';
 import { EscrowModule } from '../escrow/escrow.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { JwtModule } from '../auth/jwt.module';
 
 /**
  * Auctions Module
@@ -25,6 +26,7 @@ import { NotificationsModule } from '../notifications/notifications.module';
 @Module({
   imports: [
     ScheduleModule.forRoot(), // For auction lifecycle cron jobs
+    JwtModule, // For JWT authentication
     WalletModule, // For payment integration
     forwardRef(() => EscrowModule), // For escrow protection
     NotificationsModule, // For seller notifications
